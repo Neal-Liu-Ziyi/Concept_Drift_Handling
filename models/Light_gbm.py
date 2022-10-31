2dece521eccc4b8e756df9a8fe0534044bca347f
Neal_Liu <81135658+Neal-Liu-Ziyi@users.noreply.github.com>
2022-11-01 10:33:02 +1100

    add models

---
+++ models/Light_gbm.py
@@ -0,0 +1,23 @@
+# import library
+
+import pandas as pd
+import itertools
+import numpy as np
+import random
+from random import choices
+from pandas.plotting import autocorrelation_plot
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import seaborn as sns
+from matplotlib import pyplot
+import lightgbm as lgb
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import mean_absolute_error
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+
+
+
+
+
